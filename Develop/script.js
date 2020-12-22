688519e1bbe3e249bf43557719e412561fad09cf
Steven Ly <ly.steven16@gmail.com>
2020-12-22 17:54:51 -0600

    Added in functions to check for character types to include

--- Develop/script.js
+++ Develop/script.js
@@ -7,29 +7,96 @@
 //Prompt #2: Please choose which character types to include for your generated password. Enter '1' for lowercase, 
 // '2' for uppercase, '3' for numeric, and '4' for special characters. 
 
-var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
 
-var writePassword = function() {
-  window.alert("Welcome to Password Generator! Please answer the following prompts for password criteria.");
+var generatePassword = function() {
+  // Variables
+  var letters = "abcdefghijklmnopqrstuvwxyz"
+  var numbers = "0123456789"
+  var special = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}"
+  var criteria = []
 
-  var length = window.prompt("Please enter a number from 8 - 128 for the length of your generated password.");
-  console.log(length);
+  var confirmation = function(words, returnTrue, returnFalse, x) {
+    confirm("Are you sure you want to " + x + "include " + words + "?")
+    if (confirm) {
+      returnTrue;
+    } else {
+      returnFalse;
+    }
+  }
 
-  var charType = window.prompt("Please choose which character types to include for your generated password. Enter '1' for lowercase,'2' for uppercase, '3' for numeric, and '4' for special characters.");
+  // ask for length
+  var getLength = function() {
+    passwordLength = prompt("Please enter a number from 8 - 128 for the length of your generated password.");
+    if (!passwordLength) {
+      alert("Please enter a valid option.")
+      getLength();
+    } else if (parseInt(passwordLength) > 8 && parseInt(passwordLength) < 128) { 
+      passwordLength = parseInt(passwordLength);
+      alert("Your password length is " + passwordLength);
+    } else {
+      alert("Please enter a valid option.");
+      getLength();
+    }
+  }
+  // ask if they want lowercase
+  var getLower = function() {
+    lowerCase = confirm("Do you want to include lowercase letters in your password?");
+    if (lowerCase) {
+      confirmation("lowercase letters", criteria.push(letters), getLower(), "")
+  } else {
+      confirmation("lowercase letters", true, getLower(), "not")
+  }
 
+  // ask if they want uppercase
+  var getUpper = function() {
+    upperCase = confirm("Do you want to include uppercase letters in your password?");
+    if (upperCase) {
+      confirmation("uppercase letters", criteria.push(letters.toUpperCase()), getUpper(), "")
+  } else {
+      confirmation("uppercase letters", true, getUpper(), "not")
+  }
 
-  var pass = ''; 
-  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +  
-            'abcdefghijklmnopqrstuvwxyz0123456789@#$'; 
-      
-  for (i = 1; i <= length; i++) { 
-    var char = Math.floor(Math.random() * str.length + 1); 
-         pass += str.charAt(char);
-    } 
-    window.alert("Your generated password is: " + pass);
-    
+  // ask if they want numeric
+  var getNumeric = function() {
+    numeric = confirm("Do you want to include numbers in your password?");
+    if (numeric) {
+      confirmation("numbers", criteria.push(numbers), getNumeric(), "")
+  } else {
+      confirmation("numbers", true, getNumeric(), "not")
+  }
 
+  // ask if they want special characters
+  var getSpecial = function() {
+    specialCase = confirm("Do you want to include special characters in your password?");
+    if (specialCase) {
+      confirmation("special characters", criteria.push(special), getSpecial(), "")
+  } else {
+      confirmation("special characters", true, getSpecial(), "not")
+  }
+
+  // run functions
+  var getCriteria = function() {
+    criteria = []
+    getLength();
+    getLower();
+    getUpper();
+    getNumeric();
+    getSpecial();
+    if (!criteria) {
+      alert("You need to choose at least one character type.")
+      getCriteria();
+    }
+  }
+
+getCriteria();
+
+for (i = 0; i < passwordLength; i++) {
+  Math.floor(Math.random() * )
+}
+
+  // return password
+  return special;
 }
 
 
